19e8e4fe6d45402e7c9cc938bd522892bfb8d50b
fikyfjw <vq.rhyme@gmail.com>
2016-08-22 18:05:13 +0700

    file web crawler

---
+++ web-crawler.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+func getSiteURL(mainURL string) <-chan string {
+
+	client := &http.Client{}
+	res, err := client.Get(mainURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bodyStr := string(body[:])
+	var regex = regexp.MustCompile("http://" + "[a-z]+" + ".ub.ac.id/en")
+	var urlStr = regex.FindAllString(bodyStr, -1)
+
+	channel := make(chan string)
+	go func() {
+		defer close(channel)
+		for _, linkURL := range urlStr {
+			channel <- linkURL
+		}
+	}()
+	return channel
+
+}
+
+func main() {
+	//channel := make(chan bool)
+
+	//defer close(channel)
+
+	seedUrls := os.Args[1:]
+
+	for _, url := range seedUrls {
+		fetchURLSite(
+			getSiteURL(url))
+	}
+
+	//<-channel
+}
